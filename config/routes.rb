055f2bb2b93660f34773c3a6bee3d14171b85f0c
Daniel Coley <dmcoley948562@gmail.com>
2017-07-12 10:03:34 -0600

    Added contact page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
 
   get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
 
   resources :leads
   mount ActionCable.server => '/cable'
