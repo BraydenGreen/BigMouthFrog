aee00b22ebe7fd005c90f3316bdd6427db0ac666
Mckaid Gustafson <mckaidcgustafson@gmail.com>
2017-07-13 16:15:41 -0600

    lead_source

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170712154958) do
+ActiveRecord::Schema.define(version: 20170713213348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170712154958) do
     t.integer "contacted", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "lead_source"
   end
 
   create_table "notes", force: :cascade do |t|
