f6a3a4362fb65cff25f138fa493348f320473fbc
Aaron Anderson <aaron.russell.anderson@gmail.com>
2017-07-15 09:39:46 -0600

    updated footer with proper info and hid the featured clients page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = BigMouthFrogCopyrightTool::Renderer.copyright("Big Mouth Frog Development", "All right reserved")
+  end
+end
+
+module BigMouthFrogCopyrightTool
+  class Renderer
+    def self.copyright name, msg
+      "&copy; #{Time.now.year} | <b> #{name} </b> | #{msg}".html_safe
+    end
+  end
 end
