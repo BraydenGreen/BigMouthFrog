3251720e43c7960566dc3325f89caf21e808e1ad
jlund15 <jared.lund@icloud.com>
2017-07-18 17:43:45 +0000

    updated servername

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server '165.227.11.147', port: 22, roles: [:web, :app, :db], primary: true
+server '165.227.11.147', port: 80, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:AaronRAnderson/BigMouthFrog.git'
 set :application,     'BigMouthFrog'
