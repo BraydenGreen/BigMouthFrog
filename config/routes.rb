28d99b0575cc88f47f4fa9e357946407a2cb1d45
Elise Adams <neuman.elise@gmail.com>
2017-07-25 17:05:55 -0600

    Updated the featured clients section.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # resources :featured_clients
+ # resources :featured_clients
   resources :notes
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout'}
   root to: 'pages#home'
